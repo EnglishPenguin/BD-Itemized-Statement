d37141fcd9b21e7e4571b63a4c5c6b9fb6ad0cb0
Danielle Pashayan <daniellepashayan@gmail.com>
2024-07-17 11:36:06 -0400

    updated for server

--- run.bat
+++ run.bat
@@ -1,7 +1,16 @@
+echo "Running Itemized Statement File Generation
 @Echo off
 
 SETLOCAL
-set FILE_PATH=%~dp0
+set FILE_PATH=C:\Users\pa_dpashayan\Desktop\PyProjects\BD-Itemized-Statement\
+cd %FILE_PATH%
 set SCRIPT_PATH=%FILE_PATH%main.py
-python -u "%SCRIPT_PATH%"
+set VENV_PATH=%FILE_PATH%.venv
+
+call "%VENV_PATH%\Scripts\activate.bat"
+python -u "%SCRIPT_PATH%" > logs\TS_log.txt 2>&1
+
+IF ERRORLEVEL 1 (
+    echo Python script encountered an error. The error message is: %ERRORLEVEL% %ERRORMESSAGE%
+)
 ENDLOCAL
