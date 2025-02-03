ef7d428eadb8f87565903814f893537dc568a1b5
Danielle Pashayan <daniellepashayan@gmail.com>
2025-02-03 16:07:07 -0500

    added custom json stratus

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import BD_input_file
+from process_status.status_handler import JSONStatus
 
 if __name__ == '__main__':
-    BD_input_file.facs_report_prep()
-    BD_input_file.save_hcx_report()
+    status = JSONStatus(
+        master_file_path=r'\\NT2KWB972SRV03\SHAREDATA\CPP-Data\CBO Westbury Managers\LEADERSHIP\Bot Folder\Automated Scripts Status.json',
+        process_name='Sample Script'
+    )
+    status.update_status('Running')
+    try:            
+        BD_input_file.facs_report_prep()
+        BD_input_file.save_hcx_report()
+    except Exception as e:
+        status.update_status('Failed', errors=str(e))
